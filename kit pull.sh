57929b131e1444594bc4c6969da49d0ee5929501
jonasstr <jonas.strittmatter@gmx.de>
2019-01-06 11:29:07 +0100

    Add kit pull script

---
+++ kit pull.sh
@@ -0,0 +1,9 @@
+clear
+echo Updating /kit..
+cd C:/Users/jonas/Documents/KIT/Sonstiges
+git pull
+
+echo $'\n'Updating /assignments..
+cd C:/Users/jonas/Documents/KIT/Programmieren
+git pull
+$SHELL
