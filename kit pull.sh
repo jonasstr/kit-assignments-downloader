7f7c154b1cd8b0b92aa59b90aedc7ba6b8da4a4e
jonasstr <jonas.strittmatter@gmx.de>
2019-01-11 19:55:58 +0100

    Add example config

--- kit pull.sh
+++
@@ -1,9 +0,0 @@
-clear
-echo Updating /kit..
-cd C:/Users/jonas/Documents/KIT/Sonstiges
-git pull
-
-echo $'\n'Updating /assignments..
-cd C:/Users/jonas/Documents/KIT/Programmieren
-git pull
-$SHELL
