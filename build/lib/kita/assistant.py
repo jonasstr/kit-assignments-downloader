fb9c2d7f028b39598889f261219d57a6b9b3c6f6
jonasstr <jonas.strittmatter@gmx.de>
2019-02-02 18:10:09 +0100

    Add travis.yml

---
+++ build/lib/kita/assistant.py
@@ -0,0 +1,194 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+from colorama import Fore, Style
+from colorama import init
+import click
+
+import kita.misc.utils as utils
+
+class Assistant:
+
+    def __init__(self, yaml, dao):
+        # Initialize colorama.
+        init()
+        self.yaml = yaml
+        self.dao = dao    
+        root = tk.Tk()
+        root.withdraw()
+        root.wm_attributes("-topmost", True)
+
+
+    def echo(self, text, is_prompt=False):
+        """Forwards the given text to click.echo() and optionally applies a different style to the text."""
+        color = Fore.CYAN if is_prompt else Style.RESET_ALL
+        if is_prompt:
+            text = "> " + text
+        click.echo(color + text)
+
+
+    def prompt(self, text):
+        """Forwards the given text to click.echo() and adds cyan color and a '>' symbol to the start of the string."""
+        return click.prompt(Fore.CYAN + "> " + text)
+
+
+    def confirm(self, text, default=False):
+        suffix = " (Y/n) [y]: " if default else " (y/N) [n]: "
+        return click.confirm(Fore.CYAN + "> " + text, default=default, show_default=False, prompt_suffix=suffix)
+
+
+    def setup_user(self):
+        """Starts the setup assistant for setting up the user.yml file.    
+        Saves the login credentials of the user and the root path for downloading assignments.
+        """
+        # user.yml already exists.
+        if os.path.isfile(self.dao.user_yml_path):
+            if not self.confirm("Kita is already set up. Overwrite existing config?"):
+                return False
+    
+        self.echo("\nWelcome to the Kita 1.0.0 setup utility.\n\nPlease enter values for the following "
+            "settings (just press Enter to\naccept a default value, if one is given in brackets).\n")
+    
+        data = {}
+        data['user_name'] = self.prompt("Enter your correct ilias user name").strip()
+        data['password'] = self.prompt("Enter your ilias password").strip()
+        self.echo("\nChoose a location for saving your assignments. If you already\n"
+            "downloaded assignments manually please choose your KIT folder\nfor auto-detection.")
+        self.echo("Select the root path for your assignments from the dialog window:", is_prompt=True)
+        
+        root_path = os.path.abspath(filedialog.askdirectory())
+        data['destination'] = {}
+        data['destination']['root_path'] = root_path
+        # Set default rename format.
+        data['destination']['rename_format'] = "Blatt$$"
+        self.dao.create_user(data)
+
+        self.echo("Saved root folder '{}'.".format(utils.reformat(root_path)))
+        return True 
+
+
+    def setup_config(self):
+        """Starts the setup assistant for setting up the config.yml file."""
+        if os.path.isfile(self.dao.user_yml_path):
+            self.dao.load_user()
+
+            root_path = self.dao.user_data['destination']['root_path']
+            if not os.path.isdir(root_path):
+                self.echo("\nKita has not been configured correctly (root_path not found).\nUse 'kita setup --user' instead.")
+                return False
+    
+            assignment_folders = self.detected_assignment_folders(root_path)
+            added_courses = []
+            if assignment_folders:
+                added_courses = self.show_kit_folder_detected_dialog(assignment_folders, root_path)
+            self.show_confirm_all_courses_dialog((course for course in self.dao.config_data), added_courses, root_path)
+            return True
+    
+    
+    def show_kit_folder_detected_dialog(self, assignment_folders, root_path):    
+        """Asks the user to confirm the download locations for the given courses."""    
+        self.echo("\nPossible KIT folder detected:")
+        added_courses = []
+        for selection in assignment_folders:
+            full_path = os.path.join(root_path, selection['folder_name'])
+            message = utils.reformat("Save {} assignments to '{}'?".format(selection['course_key'].upper(), full_path))
+            if self.confirm(message, default=True):
+                added_courses.append(selection['course_key'])
+                self.dao.config_data[selection['course_key']]['path'] = selection['folder_name']
+                self.dao.dump_config()
+        return added_courses
+        
+
+    def show_confirm_all_courses_dialog(self, assignment_folders, added_courses, root_path):
+        if not added_courses:
+            download_dir = self.create_download_folder(assignment_folders, root_path)
+            click.echo("Assignments will be saved to '{}'".format(download_dir))
+            return
+        self.update_selected_courses(added_courses)
+        while self.choice and not self.confirm("Are these all courses: {}?".format(self.selected), default=True):
+            selection = self.show_select_folder_manually_dialog(self.choice, "Which courses are missing?")
+            self.show_assignments_save_location_dialog(selection)
+            added_courses.append(selection['course_key'].lower())
+            self.update_selected_courses(added_courses)
+
+
+    def update_selected_courses(self, added_courses):
+        self.selected = ', '.join(course.upper() for course in added_courses)
+        self.choice = ', '.join(key.upper() for key in self.dao.config_data.keys() if key not in added_courses)
+
+
+    def show_assignments_save_location_dialog(self, selection):
+        self.echo("{} assignments will be saved to '{}'.".format(
+            selection['course_key'].upper(), utils.reformat(selection['selected_path'])))
+        self.dao.config_data[selection['course_key']]['path'] = selection['selected_path']
+        self.dao.dump_config()
+
+
+    def show_select_folder_manually_dialog(self, choice, prompt_msg):
+        """Shows the setup dialog for adding the location of additional assignments."""
+        course_key = self.prompt("{} Choose from {}".format(prompt_msg, choice))
+        while not course_key.lower() in self.dao.config_data.keys():
+            self.echo("Error: invalid input")
+            course_key = self.prompt("{} Choose from {}".format(prompt_msg, choice))
+        self.echo("Choose a location for saving your {} courses:".format(course_key.upper()), is_prompt=True)
+        return {'course_key' : course_key, 'selected_path' : filedialog.askdirectory()}
+
+
+    def create_download_folder(self, course_keys, root_path):
+        """
+        :param course_keys: 
+        :param root_path: 
+        """
+        download_dir = os.path.join(root_path, "Downloads")
+        os.makedirs(download_dir, exist_ok=True)
+        for key in course_keys:
+            if key in self.dao.config_data:
+                new_key = self.dao.config_data[key]['name'].replace('/','-').replace('\\','-')
+                course_dir = os.path.join(download_dir, new_key)
+                os.makedirs(course_dir, exist_ok=True)
+                self.dao.config_data[key]['path'] = course_dir
+                self.dao.dump_config()
+        return download_dir
+
+
+    def detected_assignment_folders(self, root_path):
+        course_folders = next(os.walk(root_path))[1]            
+        assignment_folders = []
+        for folder_name in course_folders:
+            sub_folders = next(os.walk(os.path.join(root_path, folder_name)))[1]  
+            result = self.search_for_assignments_folder(folder_name, sub_folders)
+            if result:
+                assignment_folders.append(result)
+        return assignment_folders
+
+
+    def search_for_assignments_folder(self, folder_name, sub_folders):
+        """Searches for a possible folder containing the assignments based on the folder name."""
+        for course_key in self.dao.config_data:
+            # Folder has been found.
+            if course_key == folder_name.lower() or self.are_similar(folder_name, course_key, self.dao.config_data[course_key]['name']):
+                sub_folder_name = self.found_assignments_sub_folder(folder_name, sub_folders)
+                return ({'course_key' : course_key, 'folder_name' : sub_folder_name} if sub_folder_name else
+                    {'course_key' : course_key, 'folder_name' : folder_name})
+
+
+    def found_assignments_sub_folder(self, course_folder_name, sub_folders):
+        for sub_folder in sub_folders:
+            name_list = ['assignments', 'blätter', 'übungen', 'übungsblätter']
+            # Check whether the name of the sub-folder is either one of the above names.
+            if any(x in sub_folder.lower() for x in name_list):
+                return os.path.join(course_folder_name, sub_folder)
+
+
+    def are_similar(self, folder_name, course_key, course_name):
+        """Checks whether a given folder name is similar to the full name of a config.yml course
+                or is equal to the course key (e.g. la)."""
+        if (folder_name.lower() == course_key or folder_name.startswith(course_name) or 
+            course_name.startswith(folder_name)):
+            return True
+        course_suffixes = {'I': 1, 'II': 2, 'III': 3}
+        for suffix in course_suffixes:
+            if folder_name.endswith(suffix) or course_name.endswith(suffix):
+                return (folder_name.replace(suffix, str(course_suffixes[suffix])) == course_name or
+                    course_name.replace(suffix, str(course_suffixes[suffix])) == folder_name)
---
+++ build/lib/kita/cli.py
@@ -0,0 +1,228 @@
+import logging
+import os
+import re
+import sys
+import traceback
+
+import click
+from pathlib import Path
+from logging.handlers import RotatingFileHandler
+from ruamel.yaml import YAML
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+from kita import core
+from kita.dao import Dao
+from kita.assistant import Assistant
+import kita.misc.utils as utils
+
+print(os.path.join(Path(__file__).resolve().parents[1], "geckodriver"))
+gecko_path = os.path.join(Path(__file__).resolve().parents[1], "geckodriver")
+user_yml_path = os.path.join(click.get_app_dir("kita"), "user.yml")
+config_yml_path = os.path.join(Path(__file__).parents[0], "config.yml")
+
+yaml = YAML(typ='rt')
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.compact(seq_seq=False, seq_map=False)
+
+# Create data access object and load data on startup.
+dao = Dao(gecko_path, user_yml_path, config_yml_path, yaml)
+dao.load_data(suppress_access_errors=True)
+
+def get_options():
+    """Creates Firefox options for running kita in headless mode."""
+    options = Options()
+    options.headless = True
+    return options
+
+
+def is_positive_int(value):
+    """
+
+    :param value: 
+
+    """
+    return re.search('^\d+$', value)
+
+
+def is_range(value):
+    """
+
+    :param value: 
+
+    """
+    return re.search('^\d+-\d+$', value)
+
+
+def is_sequence(value):
+    """
+
+    :param value: 
+
+    """
+    return re.search('^\d+(?:,\d+)*$', value)
+
+def print_info(ctx, param, value):
+
+    if not value or ctx.resilient_parsing:
+        return
+    
+    click.echo("Current user: {}".format(dao.user_data['user_name']))
+    click.echo("Root path: {}\n".format(dao.user_data['destination']['root_path']))
+    added_courses = dao.added_courses()
+    click.echo("Added courses:")
+    for course in added_courses:
+        click.echo("{}: {}".format(course.upper(), utils.reformat(dao.config_data[course]['path'])))
+    available_courses = ', '.join(course.upper() for course in dao.config_data if course not in added_courses)
+    click.echo("\nAvailable courses: {}".format(available_courses))
+    ctx.exit()   
+
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option(message='%(prog)s version %(version)s')
+@click.option('--info', '-i', is_flag=True, callback=print_info,
+    expose_value=False, help="Show information about the current user and courses and exit.")
+@click.pass_context
+def cli(ctx):
+    """Thank you for using the KIT Assignments Downloader!
+    
+    In order to download assignments make sure the setup was successful
+    (otherwise run 'kita setup' again).\n
+    To get started, just use the 'kita update la' command
+    where 'la' is one of your courses.\n
+    If you only want to download a specific assignment, use
+    'kita get la 9' where '9' is the assignment number.\n   
+    In case the download isn't working or you encounter any
+    bugs/crashes please visit github.com/jonasstr/kita and
+    create an issue or contact me via email: uzxhf@student.kit.edu.
+    """
+    if str(ctx.invoked_subcommand) is not 'setup':
+        # Make sure user.py has been created and config.yml exists.
+        if setup_incorrectly("user.yml", user_yml_path) or setup_incorrectly("config.yml", config_yml_path):
+            sys.exit(1)
+
+
+def setup_incorrectly(file_name, path):
+    if not os.path.isfile(path):
+        click.echo("\nKita has not been configured correctly ({} not found)."
+            "\nUse 'kita setup' before downloading assignments.".format(file_name))
+        return True
+
+
+@cli.command()
+@click.option('--config', '-cf', is_flag=True, help="Change the download locations for the courses.")
+@click.option('--user', '-u', is_flag=True, help="Change the current user and the root path for downloads.")
+def setup(config, user):
+    """Start the command line based setup assistant or change previous settings."""
+
+    assistant = Assistant(yaml, dao)
+    # Setup user.yml if either the --user option has been provided or no options at all.
+    if user or user == config:
+        if not assistant.setup_user():
+            click.echo("Setup cancelled.")
+            return
+    # Setup config.yml if either the --config option has been provided or no options at all.
+    if config or user == config:
+        if not assistant.setup_config():
+            click.echo("Setup cancelled.")
+            return
+    click.echo("\nSetup successful. Type 'kita --help' for details.")
+
+
+def add(config):
+    """Add a new course to the list of supported courses."""
+
+def create_profile():
+    """Create a Firefox profile required for navigating on a webpage.
+    
+    Set the preferences allowing PDFs to be downloaded immediately as well as
+    navigating between tabs using keyboard shortcuts.
+
+    :returns: The Firefox profile.
+    """
+    profile = webdriver.FirefoxProfile()
+    # Set download location
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.dir", dao.user_data['destination']['root_path'])
+    # Close download window immediately
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.manager.closeWhenDone", True)
+    # Download PDF files without asking the user
+    profile.set_preference("pdfjs.disabled", True)
+    profile.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+    # Don't switch between recently visited tabs
+    profile.set_preference("browser.ctrlTab.recentlyUsedOrder", False)
+    # Move directly to newly opened tab
+    profile.set_preference("browser.tabs.loadInBackground", False)
+    return profile
+
+
+def create_scraper(headless, verbose):
+    driver = webdriver.Firefox(firefox_profile=create_profile(),
+        executable_path=gecko_path,
+        options=get_options() if headless else None)
+    return core.Scraper(driver, dao, verbose)
+
+
+@cli.command()
+@click.argument('course_names', nargs=-1, required=True, type=click.Choice(dao.config_data))
+@click.argument('assignment_num')
+@click.option('--move/--keep', '-mv/-kp', default=True, help="Move the downloaded assignments to their course directory"
+    " (same as 'kita update') or keep them in the browser's download directory (default: move).")
+@click.option('--all', '-a', is_flag=True, help="Download assignments from all specified courses.")
+@click.option('--headless/--show', '-hl/-s', default=True, help="Start the browser in headless mode (no visible UI).")
+@click.option('--verbose', '-v', is_flag=True, help="Print additional information during the download process.")
+def get(course_names, assignment_num, move, all, headless, verbose):
+    """Download one or more assignments from the specified course(s) and move them into the correct folders."""
+
+    if is_positive_int(assignment_num):
+        assignments = [assignment_num]
+    # Alternative range for assignment_num instead of int. Example: 5-10
+    elif is_range(assignment_num):
+        assignment_nums = assignment_num.split('-')
+        assignments = range(int(assignment_nums[0]), int(assignment_nums[1]) + 1)
+    # Alternative int sequence for assignment_num instead of int. Example: 5,10,11
+    elif is_sequence(assignment_num):
+        assignments = assignment_num.split(',')
+    else: 
+        print("Assignment number must be an integer or in the correct format!")
+        return
+    
+    scraper = create_scraper(headless, verbose)
+    courses_to_iterate = dao.config_data if all else course_names
+    
+    for name in courses_to_iterate:
+        course_ = dao.config_data[name]
+        for num in assignments:
+            scraper.get(course_, num, move)
+    scraper.driver.quit()
+    
+
+@cli.command()
+@click.argument('course_names', nargs=-1, required=False, type=click.Choice(dao.config_data))
+@click.option('--all', '-a', is_flag=True, help="Update assignment directories for all specified courses.")
+@click.option('--headless/--show', '-hl/-s', default=True, help="Start the browser in headless mode (no visible UI).")
+@click.option('--verbose', '-v', is_flag=True, help="Print additional information during the download process.")
+def update(course_names, all, headless):
+    """Update one or more courses by downloading the latest assignments."""
+    
+    scraper = create_scraper(headless, verbose)
+    all = True if not course_names else all
+    courses_to_iterate = dao.config_data if all else course_names
+    
+    for name in courses_to_iterate:
+        try:
+            course_ = dao.config_data[name]
+            scraper.update_directory(course_, name)
+        except (IOError, OSError):
+            raise
+            print("Invalid destination path for this assignment!")
+        except:
+            raise
+            print("Assignment not found!")
+    scraper.driver.quit()    
+
+
+if __name__ == '__main__':
+    cli()
